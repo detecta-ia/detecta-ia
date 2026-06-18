400d94000291a0cef19ceb3ec838d6f9b25f0509
rick8935 <senac.rickboone@gmail.com>
2026-06-18 20:00:34 -0300

    feat #26: adiciona função Cadastro de Novo Produto

---
+++ ApiExemplo/backend/src/BaseApi.Application/Produtos/Commands/AtualizarProduto/AtualizarProdutoHandler.cs
@@ -0,0 +1,35 @@
+﻿using BaseApi.Domain.Interfaces.Repositorios;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BaseApi.Application.Produtos.Commands.AtualizarProduto
+{
+    public class AtualizarProdutoHandler : IRequest<Unit>
+    {
+        private readonly IProdutoRepositorio _repositorio;
+
+        public AtualizarProdutoHandler(IProdutoRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public async Task<Unit> Handle(AtualizarProdutoCommand command, CancellationToken ct)
+        {
+            var produto = await _repositorio.ObterPorIdAsync(command.Id, ct)
+                ?? throw new Exception($"Produto com Id '{command.Id}' não encontrado.");
+
+            produto.Nome = command.Nome;
+            produto.Categoria = command.Categoria;
+            produto.Preco = command.Preco;
+
+            produto.AtualizadoEm = DateTime.UtcNow;
+
+            _repositorio.Atualizar(produto);
+            await _repositorio.SalvarAsync(ct);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ ApiExemplo/backend/src/BaseApi.Application/Produtos/Queries/ObterProdutoPorId/ObterProdutoPorIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseApi.Application.Produtos.Queries.ObterProdutoPorId;
+
+public record ObterProdutoPorIdQuery(Guid Id) : IRequest<ProdutoDetalheDto>;
+
+public record ProdutoDetalheDto(
+    Guid Id,
+    string Nome,
+    decimal Preco,
+    DateTime CriadoEm,
+    DateTime AtualizadoEm
+);
+
